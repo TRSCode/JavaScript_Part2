5f21a23698ebc0359ac5580625dbe725e9401854
Tony Schock <tony.schock@gmail.com>
2023-05-05 16:28:23 -0600

    initial commit

---
+++ myNewProject/README.md
@@ -0,0 +1,10 @@
+# myNewProject
+## Full-Stack MERN
+
+This is a code along with the learning platform to complete a full-stack application using the following:
+* JavaScript
+* React
+* Axios
+* MongoDB
+* Mongoose
+* Express
---
+++ myNewProject/server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());                           /* This is new and allows JSON Objects to be posted */
+app.use(express.urlencoded({ extended: true }));   /* This is new and allows JSON Objects with strings and arrays*/
+require('./config/mongoose.config');    /* This is new */
+require('./routes/person.routes')(app);
+// These two lines are the long-hand notation of the code above. They better show what's going on.
+    // const personRoutes = require("./routes/person.routes");  <-- assign the exported function to a const
+    // personRoutes(app);     <-- invoke the function and pass in the express "app" server  
+
+
+
+
+app.listen(port, () => console.log(`Listening on port: ${port}`) );
