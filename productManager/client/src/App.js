d11d94346f3bca7a57524d05a6677cea9be92f15
Tony Schock <tony.schock@gmail.com>
2023-05-10 16:27:02 -0600

    added redirect route to home

--- productManager/client/src/App.js
+++ productManager/client/src/App.js
@@ -3,14 +3,15 @@ import React from 'react';
 import Main from './views/Main';
 import Detail from './components/Detail';
 import Update from './components/Update';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       {/* <ProductForm/> */}
       <BrowserRouter>
         <Routes>
-          <Route element={<Main/>} path="/home"default/>
+          <Route element={<Main/>} path="/home"/>
+          <Route element={<Navigate to="/home" />} path="/" />
           <Route element={<Detail/>} path="/product/:id"/>
           <Route element={<Update/>} path="/product/edit/:id"/>
         </Routes>
