7174265c304ed2cfce41eb3324a24a15e0247b37
Tony Schock <tony.schock@gmail.com>
2023-05-10 13:40:11 -0600

    Added Delete to Detail component

--- productManager/client/src/components/Detail.jsx
+++ productManager/client/src/components/Detail.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import {useParams, Link} from "react-router-dom";
+import {useParams, Link, useNavigate} from "react-router-dom";
 
 const Detail = (props) => {
     const [ product, setProduct] = useState({})
     const {id} = useParams();
+    const navigate = useNavigate();
+
     useEffect(() => {
         axios.get("http://localhost:8000/api/product/" + id)
             .then(res => {
@@ -14,13 +16,14 @@ const Detail = (props) => {
             .catch( err => console.log(err));
     }, []);
 
-    // const deleteProduct = (productId) => {
-    //     axios.delete('http://localhost:8000/api/product/' + productId)
-    //         .then(res => {
-    //             removeFromDom(productId)
-    //         })
-    //         .catch(err => console.log(err))
-    // }
+    const deleteProduct = () => {
+        axios.delete('http://localhost:8000/api/product/' + id)
+            .then(res => {
+                console.log(res.data);
+                navigate('/home');
+            })
+            .catch(err => console.log(err))
+    }
 
     return (
         <div className="container col-5 mt-3">
@@ -29,9 +32,9 @@ const Detail = (props) => {
             <p>Description: {product.description}</p>
             <Link to={'/home'}> Home |</Link>
             <Link to={'/product/edit/' + product._id}> Edit</Link>
-            {/* <button onClick={(e)=>{deleteProduct(product._id)}} className="btn btn-danger ms-2">
+            <button onClick={deleteProduct} className="btn btn-danger ms-2">
                 Delete
-            </button> */}
+            </button>
         </div>
     );
 }
